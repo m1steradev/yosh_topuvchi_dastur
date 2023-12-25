c8f8addc5fecffb8279c1eb52426575d93850822
Mr.Adev <154827603+m1steradev@users.noreply.github.com>
2023-12-25 22:16:32 +0500

    Add files via upload

---
+++ yosh_topuvchi_dastur.py
@@ -0,0 +1,7 @@
+yosh = int(input("Yoshingiz nechida? "))
+f_yoshi = 2023 - yosh
+print("Siz " , f_yoshi , " yilda tug'ilgansiz!")
+
+yosh = int(input("Yoshingiz nechida? \n>>>"))
+t_yil = 2020-yosh
+print("Siz ", t_yil, " da tug'ilgansiz")c
